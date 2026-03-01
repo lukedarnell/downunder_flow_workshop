b647b28f1c8f2707bb35539c077526689bb5aca8
Luke Darnell <lukedarnell@hotmail.com>
2026-03-01 14:21:53 +1100

    Fixing up the tests.

--- src/project.v
+++ src/project.v
@@ -18,7 +18,7 @@ module tt_um_explorer (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 8'b01101010;
+  assign uio_out = 8'd50;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
